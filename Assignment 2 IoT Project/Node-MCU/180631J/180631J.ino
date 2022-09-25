4fee90230efcad4e82779abf56a724094e46d4e7
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2022-09-25 14:45:36 +0530

    verified functionality when threshold is passed by a parameter

--- Assignment 2 IoT Project/Node-MCU/180631J/180631J.ino
+++ Assignment 2 IoT Project/Node-MCU/180631J/180631J.ino
@@ -1,13 +1,13 @@
 /*
 *************************************************************************************
-*                                Author Details                                     *
-* Author: Thalagala B. P.   180631J                                                 *
-* Github: https://github.com/bimalka98                                              *
-* Email: bimalkapiyaruwan1998322@gmail.com                                          *
-*===================================================================================*
+                                 Author Details
+  Author: Thalagala B. P.   180631J
+  Github: https://github.com/bimalka98
+  Email: bimalkapiyaruwan1998322@gmail.com
+  ===================================================================================
 */
 
-// include the necessary libraries 
+// include the necessary libraries
 #include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
 #include <PubSubClient.h> // https://github.com/knolleary/pubsubclient
 
@@ -15,6 +15,7 @@
 // initialize the global variables
 WiFiClient ESPClient;
 PubSubClient mqttClient(ESPClient);
+String WarnInterval = "";
 
 
 // user defined fucntion declaration
@@ -22,7 +23,7 @@ void receiveCallback(char * topic, byte* payload, unsigned int length);
 void connectToBroker();
 /*
 *************************************************************************************
-*                >>>       Start of Setup Function       >>>                        *
+                 >>>       Start of Setup Function       >>>
 *************************************************************************************
 */
 
@@ -37,7 +38,7 @@ void setup() {
 
   // reset settings - wipe stored credentials for testing
   // these are stored by the esp library
-  wm.resetSettings();
+  //  wm.resetSettings();
 
   // Automatically connect using saved credentials if avilable,
   // if connection fails, starts an access point with the following credentials
@@ -46,7 +47,7 @@ void setup() {
 
   // goes into a blocking loop awaiting configuration and will return success result
   if (!res) {
-    Serial.println("[ERROR] Failed to connect");    
+    Serial.println("[ERROR] Failed to connect");
   }
   else {
     //if you get here you have connected to the WiFi
@@ -57,28 +58,42 @@ void setup() {
   mqttClient.setServer("test.mosquitto.org", 1883);
   mqttClient.setCallback(receiveCallback);
 
+  // for warning mechanism
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
 }
 
 /*
 *************************************************************************************
-*                     >>>         Start of Loop         >>>                         *
+                      >>>         Start of Loop         >>>
 *************************************************************************************
 */
 
 void loop() {
   // main code, to run repeatedly:
-  if(!mqttClient.connected()) {
+  if (!mqttClient.connected()) {
     connectToBroker();
   }
 
   // checking any incoming messages for callback
   mqttClient.loop();
-  delay(1000);
+  
+  // convert warning interval to an integer
+  if (WarnInterval != "") {
+    int warningDelay = WarnInterval.toInt();
+    digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+    delay(100);                      // Wait for a some milisecond
+    digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+    delay(warningDelay);                      // Wait for two seconds (to demonstrate the active low LED)
+  }else{
+    delay(1000);
+  }
 }
 
 /*
 *************************************************************************************
-*            >>>             User Defined Functions           >>>                   *
+             >>>             User Defined Functions           >>>
 *************************************************************************************
 */
 
@@ -87,20 +102,21 @@ void receiveCallback(char * topic, byte* payload, unsigned int length) {
   Serial.print("[INFO] message received [");
   Serial.print(topic);
   Serial.print("] ");
-
-  for(int i= 0; i < length; i++){
+  WarnInterval = "";
+  for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
+    WarnInterval += (char)payload[i];
   }
 }
 
 void connectToBroker() {
-  while(! mqttClient.connected()){
+  while (! mqttClient.connected()) {
     Serial.println("[INFO] attempting MQTT connection...");
 
-    if(mqttClient.connect("ESP-8266-180631J")){ // just a random string for authentication purposes
+    if (mqttClient.connect("ESP-8266-180631J")) { // just a random string for authentication purposes
       Serial.println("[INFO] connected to MQTT broker.");
-      mqttClient.subscribe("180631J");      
-    }else{
+      mqttClient.subscribe("180631J");
+    } else {
       Serial.println("[ERROR] failed to connect.");
       Serial.println(mqttClient.state());
       delay(5000);
